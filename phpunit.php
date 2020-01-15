b4a91a1dd4f24ed03d75ce0cb3df6af6ecc37ed1
Mark Beech <mbeech@mark-beech.co.uk>
2020-01-15 21:13:39 +0000

    Tweak

--- phpunit.php
+++ phpunit.php
@@ -13,18 +13,3 @@
 */
 
 require __DIR__.'/vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Set The Default Timezone
-|--------------------------------------------------------------------------
-|
-| Here we will set the default timezone for PHP. PHP is notoriously mean
-| if the timezone is not explicitly set. This will be used by each of
-| the PHP date and date-time functions throughout the application.
-|
-*/
-
-date_default_timezone_set('UTC');
-
-Carbon\Carbon::setTestNow(Carbon\Carbon::now());
